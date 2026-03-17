d66f41b0128e344f806a40862483873637f0c18f
HuyBin <huynhthanhhuy.ag667@gmail.com>
2026-03-17 10:35:29 +0700

    'updateFinally'

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const About = () => {
     return (
-        <div className='flex flex-col items-center justify-center 
+        <div className='flex flex-col items-center justify-center mx-auto
     container p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
             <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
             <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About Properties, Dedicated to Your Vision</p>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <div className='min-h-screen mb-4 bg-cover bg-center flex 
     items-center w-full overflow-hidden' style=
-    {{backgroundImage:"url('/header_img.png')"}} id='Header'>
-      <NavBar/>
-      <div className='container mx-atuo text-center py-4 px-6 md:px-20 lg:px-32 text-white'>
+      {{ backgroundImage: "url('/header_img.png')" }} id='Header'>
+      <NavBar />
+      <div className='container mx-auto text-center py-4 px-6 md:px-20 lg:px-32 text-white'>
         <h2 className='text-5xl sm:text-6xl md:text-[74px] max-w-3xl font-semibold pt-20 inline-block'>
           Explore homes that fit your dreams</h2>
         <div className='space-x-6 mt-16'>
